e2967dc4acf2a7f4db5f9352e7b9cb42a7c7b813
timj76 <timjetten@foni.net>
2017-08-26 12:19:33 +0200

    Umsetzung AP erster Stand und GitHub Test

---
+++ SpectraView/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpectraView
+{
+
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       // OpenFileDialog openFileDialog;
+        string Bildname = @"";
+        double höhe = 0;
+        double breite = 0;
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                Bildname = openFileDialog.FileName;
+                hauptImage.Source = new BitmapImage(new Uri(Bildname));
+
+
+        }
+
+        private void slider_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            double i = slider.Value;
+            double j = slider.PreviewMouseWheel(ManipulationDeltaEventArgs )
+            höhe = hauptImage.Height;
+            breite = hauptImage.Width;
+
+            hauptImage.Height = höhe + i;
+            hauptImage.Width = breite + i;
+
+
+        }
+
+        private void slider_MouseWheel_1(object sender, MouseWheelEventArgs e)
+        {
+
+        }
+    }
+
+  
+}
+        
+    
---
+++ SpectraView/UserControl1.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpectraView
+{
+    /// <summary>
+    /// Interaktionslogik für UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        private OpenFileDialog openFileDialog;
+
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            {
+                openFileDialog = new OpenFileDialog();
+                openFileDialog.Title = "Bild öffnen";
+                openFileDialog.Multiselect = false;
+                openFileDialog.CheckFileExists = true;
+                string dateiname = @"";
+                openFileDialog.Filter = "Image files (*.png;*.jpeg)|*.png;*.jpeg|All files (*.*)|*.*";
+                openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+                openFileDialog.FilterIndex = 5; // Hier: Alle Bilddateien
+
+
+                if (openFileDialog.ShowDialog() == true)
+                    dateiname = openFileDialog.FileName; 
+                    image.Source = new BitmapImage(new Uri(dateiname));
+
+
+            }
+        }
+    }
+}
